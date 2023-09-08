f390d7692ffa9dbb7ca1dbb81966df038247caab
Strekh <you@example.com>
2023-09-08 13:24:25 +0300

    изменил IP скан

--- Python2/1_ip_scan.py
+++ Python2/1_ip_scan.py
@@ -2,17 +2,27 @@
 проверяет их доступность с помощью ping-запросов. Результаты
 проверки сохраняются в отдельный файл. """
 
-import subprocess # Библиотека для пинга
+import subprocess
+from datetime import datetime
+from pathlib import Path
 
-# Список IP-адресов для проверки
-ips = ['192.168.20.1', '127.0.0.1', '8.8.8.8']
-log_ip_scan = open('log_ip_scan.log', 'w',)
-for ip in ips:
-    try:
-        subprocess.check_output(['ping', '-c1', ip])
-        print(ip + ' Доступен')
-        log_ip_scan.write( ip + ' Доступен \n')
-    except subprocess.CalledProcessError:
-            print(ip + ' Не доступен')
-            log_ip_scan.write(ip + ' Не доступен \n')
-            log_ip_scan.close
+ips = ['8.8.8.8', '8.8.4.4', '192.168.1.1']
+
+# Форматируем дату и время для записи в файл
+current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Открываем файл для записи
+log_ip_scan = Path("./log_ip_scan.log")
+if not log_ip_scan.exists():
+    log_ip_scan.touch()
+with open(log_ip_scan, "a") as f:
+    f.write('\n \n \n==========\n' + current_datetime)
+
+for ping_process in ips:
+    command = ['ping', '-c2', '-w10', '-n']
+    process = subprocess.run(command + ips , stdout=subprocess.PIPE)
+    output = process.stdout.decode()
+    print(output)
+    log_ip_scan = open('log_ip_scan.log', 'a',)
+    log_ip_scan.write( '\n==========\n' + output)
+    log_ip_scan.close
